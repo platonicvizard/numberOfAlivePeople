a2cc8d9602bafe5feba7ca1c4d3656753421e065
dlfrontend <dlfrontend@outlook.com>
2019-05-14 05:32:28 -0400

    started updating to convert to CS

---
+++ NumerOfAlivePeopleCS/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace NumerOfAlivePeopleCS
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var other = new OtherProgram();
+            other.Main(new string[] { "", "", "" }, (i) => {
+                Console.WriteLine($"Index({i})");
+            });
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public class OtherProgram
+    {
+        public delegate void CallBack(int i);
+
+        public void Main(String[] args, CallBack callBack)
+        {
+            
+            if (args == null)
+            {
+                return;
+            }
+
+            for (int i = 0; i < args.Length; i++)
+            {
+                callBack(i);
+            }
+            Console.WriteLine("Other Program CallBack");
+        }
+    }
+}
